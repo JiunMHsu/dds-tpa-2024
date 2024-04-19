630d018a5835abe03a1af1af9042c3787674ecef
Jiun Ming Hsu <hsujm219@gmail.com>
2024-04-19 05:20:24 -0300

    create class ValidadorDeContrasenias

---
+++ src/main/java/validador/ValidadorDeContrasenias.java
@@ -0,0 +1,44 @@
+package validador;
+
+/**
+ * Validador de contraseñas según las recomendaciones
+ * de la Sección 5.1.1.2 para Secretos Memorizados de la Guía NIST 800-63.
+ * <p/>
+ * Véase: <a href="https://pages.nist.gov/800-63-3/sp800-63b.html#memsecret"> Memorized Secret Verifiers </a>
+ */
+public class ValidadorDeContrasenias {
+
+  // TODO - esValida()
+  public boolean esValida(String unaClave) {
+    return true;
+  }
+
+  // TODO - tieneMasDeOchoCaracteres()
+  private boolean tieneMasDeOchoCaracteres(String unaClave) {
+    return true;
+  }
+
+  // TODO - esFuerte()
+  private boolean esFuerte(String unaClave) {
+    // chequeo de las 10000 peores contraseñas.
+    return true;
+  }
+
+  // TODO - noTieneCaracteresRepetidosConsecutivos()
+  private boolean noTieneCaracteresRepetidosConsecutivos(String unaClave) {
+    // si tiene secuencias tipo aaa, &&&, FFF, no va
+    return true;
+  }
+
+  // TODO - esDesordenadoAlfabeticamente()
+  private boolean esDesordenadoAlfabeticamente(String unaClave) {
+    // si tiene secuencias tipo abc, fgh, cba, no va
+    return true;
+  }
+
+  // TODO - esDesordenadoNumericamente()
+  private boolean esDesordenadoNumericamente(String unaClave) {
+    // si tiene secuencias tipo 123, 345, 876, no va
+    return true;
+  }
+}
