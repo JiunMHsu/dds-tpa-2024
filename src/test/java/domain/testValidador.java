27a4b93b71f38df316bb3cb978623d130e818194
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-04-21 12:21:43 -0300

    Test fuerte y parte consec

--- src/test/java/domain/testValidador.java
+++ src/test/java/domain/testValidador.java
@@ -1,13 +1,15 @@
 package domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import validador.ValidadorDeContrasenias;
 public class testValidador {
 
     ValidadorDeContrasenias validador = new ValidadorDeContrasenias();
     @Test
-    public void unaClaveEsFuerteSiNoPerteneceALas10000Debiles() {
+    @DisplayName("Una clave es fuerte si no pertenece a las 10000 claves debiles")
+    public void esFuerte() {
         String unaClaveDebil = "dragon";
         String unaClaveFuerte = "gloria";
         Assertions.assertFalse(validador.esFuerte(unaClaveDebil));
@@ -15,7 +17,8 @@ public class testValidador {
     }
 
     @Test
-    public void unaClaveNoTieneCaracteresRepetidosConsecutivos(){
-        String unaClaveCaracteresRePetidosConsecutivos = "fraaaa";
+    @DisplayName("Una clave no tiene caracteres repetidos consecutivos cuando posee 3 o mas")
+    public void noTieneCaracteresRepetidosConsecutivos(){
+        String claveCaracRepetConsec = "fraaaa";
     }
 }
