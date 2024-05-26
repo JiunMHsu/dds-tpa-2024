9fc29ed42bc84a7699428bd64b8f5aefe9dc8f65
Jiun Ming Hsu <hsujm219@gmail.com>
2024-05-26 19:47:18 -0300

    implementacion Pregunta

--- src/main/java/ar/edu/utn/frba/dds/models/formulario/Pregunta.java
+++ src/main/java/ar/edu/utn/frba/dds/models/formulario/Pregunta.java
@@ -1,15 +1,10 @@
 package ar.edu.utn.frba.dds.models.formulario;
 
-import java.util.*;
-
-/**
- * 
- */
 public class Pregunta {
-
-    public Pregunta() {
-    }
 
     private String contenido;
 
+    public Pregunta(String pregunta) {
+        this.contenido = pregunta;
+    }
 }
