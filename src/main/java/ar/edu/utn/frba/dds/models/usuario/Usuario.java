ca93cb8e2b84b72bef6901d50585a630cc1fc0cb
Leonel Juncos Mieres <129603152+leoojuncos@users.noreply.github.com>
2024-05-27 15:49:54 -0300

    Anana Cooked

--- src/main/java/ar/edu/utn/frba/dds/models/usuario/Usuario.java
+++ src/main/java/ar/edu/utn/frba/dds/models/usuario/Usuario.java
@@ -1,16 +1,20 @@
 package ar.edu.utn.frba.dds.models.usuario;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.*;
 
-/**
- * 
- */
+@Getter
+@Setter
+
 public class Usuario {
 
     private String nombreUsuario;
     private String contrasenia;
     private String email;
-    public Usuario() {
+
+    public Usuario(String nombreUsuario, String contrasenia, String email) {
     }
 
 }
