9bac06101c8dac5f6dc7b0b2708d8775d39d3256
Jiun Ming Hsu <hsujm219@gmail.com>
2024-05-27 14:16:59 -0300

    implementacion data

--- src/main/java/ar/edu/utn/frba/dds/models/data/Documento.java
+++ src/main/java/ar/edu/utn/frba/dds/models/data/Documento.java
@@ -1,23 +1,16 @@
 package ar.edu.utn.frba.dds.models.data;
 
-import java.util.*;
-
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-
 public class Documento {
 
     private Integer numero;
 
-    /**
-     * 
-     */
     private TipoDocumento tipo;
 
-    public Documento() {
+    public Documento(Integer numero, TipoDocumento tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
     }
-
 }
