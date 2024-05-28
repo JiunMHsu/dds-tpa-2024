445d421bf7f96a5afa4f3566c435ec06916f1836
Melselep <130233474+Melselep@users.noreply.github.com>
2024-05-27 21:44:41 -0300

    tarjetas

--- src/main/java/ar/edu/utn/frba/dds/models/tarjeta/Tarjeta.java
+++ src/main/java/ar/edu/utn/frba/dds/models/tarjeta/Tarjeta.java
@@ -1,14 +1,38 @@
 package ar.edu.utn.frba.dds.models.tarjeta;
 
+import ar.edu.utn.frba.dds.models.heladera.Heladera;
 import ar.edu.utn.frba.dds.models.usuario.PersonaVulnerable;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tarjeta {
   private String codigo;
   private PersonaVulnerable persona;
   private Integer usosDia;
-  private LocalDate fechaUltimoUso;
+  private Integer usosPorDia;
+  private LocalTime horaMedianoche = LocalTime.of(0,0,0);
+  private List<RegistroTarjetas> registro = new ArrayList<>();
+
   public Boolean puedeUsar(){
-    return null;
+    if (usosDia > 0) {
+      usosDia--;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public void registrarUsoTarjeta(Heladera heladera){
+    if(puedeUsar()){
+      RegistroTarjetas nuevoRegistro = new RegistroTarjetas(heladera);
+      registro.add(nuevoRegistro);
+    }
+  }
+  public void verificarMedianoche() {
+    if (LocalTime.now().equals(horaMedianoche)) {
+      usosDia = usosPorDia;
+    }
   }
 }
