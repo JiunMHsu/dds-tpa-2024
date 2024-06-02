31e0e53258f9ebf3287f1288e0af4937a7476722
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-06-02 00:13:42 -0300

    cambios en calcular puntos y test para puntos tarjeta

--- src/main/java/ar/edu/utn/frba/dds/models/calculadorDePuntos/PuntosDonacionViandas.java
+++ src/main/java/ar/edu/utn/frba/dds/models/calculadorDePuntos/PuntosDonacionViandas.java
@@ -1,16 +1,19 @@
 package ar.edu.utn.frba.dds.models.calculadorDePuntos;
 
-import ar.edu.utn.frba.dds.models.colaboracion.Colaboracion;
 import ar.edu.utn.frba.dds.models.colaboracion.DonacionVianda;
 import ar.edu.utn.frba.dds.models.usuario.Persona;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
-
-public class PuntosDonacionViandas {
+@Setter
+public class PuntosDonacionViandas implements CalculadorDe{
     private static double variable = 1.5;
+    private List<DonacionVianda> listaViandasDonadas = new ArrayList<>();
 
-    public static Double calcularPuntos(Persona persona){
-        List<DonacionVianda> listaViandasDonadas = Colaboracion.obtenerViandasDonadas(persona);
+    public Double calcularPuntos(Persona persona){
+        // TODO cuando veamos base de datos
         int viandasDonadas = listaViandasDonadas.size();
         return viandasDonadas * variable;
     }
