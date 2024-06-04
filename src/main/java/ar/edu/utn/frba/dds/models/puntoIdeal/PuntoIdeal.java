c6b928d265444864c02aaebdf04ded3af9dfc082
Jiun Ming Hsu <hsujm219@gmail.com>
2024-06-04 10:18:27 -0300

    test puntoIdeal

--- src/main/java/ar/edu/utn/frba/dds/models/puntoIdeal/PuntoIdeal.java
+++ src/main/java/ar/edu/utn/frba/dds/models/puntoIdeal/PuntoIdeal.java
@@ -1,13 +1,16 @@
 package ar.edu.utn.frba.dds.models.puntoIdeal;
 
 import ar.edu.utn.frba.dds.models.data.Ubicacion;
-import ar.edu.utn.frba.dds.models.heladera.Heladera;
-
 import java.util.List;
 
 public class PuntoIdeal {
     private  AdapterPuntoIdeal adapterPuntoIdeal;
-    public List<Ubicacion> puntoIdeal(Ubicacion ubicacion, Integer radio)  {
-        return adapterPuntoIdeal.puntoIdeal(ubicacion.getLatitud(), ubicacion.getLongitud(), radio);
+
+    public PuntoIdeal(AdapterPuntoIdeal adapterPuntoIdeal) {
+        this.adapterPuntoIdeal = adapterPuntoIdeal;
+    }
+
+    public List<Ubicacion> puntosIdeales(Ubicacion ubicacion, Integer radio)  {
+        return this.adapterPuntoIdeal.puntoIdeal(ubicacion.getLatitud(), ubicacion.getLongitud(), radio);
     }
 }
