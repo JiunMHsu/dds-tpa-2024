ace232109791ddde6b424566d5efd1b532bda416
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-07-12 11:01:53 -0300

    tests, error

--- src/test/java/ar/edu/utn/frba/dds/TestCalculadorDePuntos.java
+++ src/test/java/ar/edu/utn/frba/dds/TestCalculadorDePuntos.java
@@ -1,42 +1,117 @@
 package ar.edu.utn.frba.dds;
 
+import ar.edu.utn.frba.dds.models.colaboracion.*;
+import ar.edu.utn.frba.dds.models.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.models.colaborador.Usuario;
+import ar.edu.utn.frba.dds.models.heladera.CapacidadExcedidaException;
+import ar.edu.utn.frba.dds.models.heladera.EstadoHeladera;
+import ar.edu.utn.frba.dds.models.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.puntosDeColaboracion.PuntosPorColaboracion;
+import ar.edu.utn.frba.dds.repository.colaboracion.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class TestCalculadorDePuntos {
 
+  Colaborador persona = Colaborador.humana(null, null,null,null);
+  PuntosPorColaboracion calculadorDePuntos = PuntosPorColaboracion.of(persona);
   @BeforeEach
-  public void setup() {
-  }
+  public void setup() throws CapacidadExcedidaException {
+    // Donacion Dinero. Los puntos por donar dinero es la cantidad de dinero donado multiplicado por 0.5
+    // Puntos : (500+200+10000)*0.5 = 5350
+    DonacionDinero donacionDinero1 =  DonacionDinero.by(persona, 500);
+    DonacionDinero donacionDinero2 = DonacionDinero.by(persona, 200);
+    DonacionDinero donacionDinero3 = DonacionDinero.by(persona, 10000);
 
-  @Test
-  @DisplayName("Los puntos por repartir tarjetas es la cantidad de tarjetas repartidas multiplicado por 2")
-  public void puntosTarjetas() {
-  }
+    DonacionDineroRepository.agregar(donacionDinero1);
+    DonacionDineroRepository.agregar(donacionDinero2);
+    DonacionDineroRepository.agregar(donacionDinero3);
 
-  @Test
-  @DisplayName("Los puntos por donar dinero es la cantidad de dinero donado multiplicado por 0.5")
-  public void puntosDonacionDinero() {
-  }
+    // Distribucion Viandas. Los puntos por distribuir viandas es la cantidad de viandas distribuidas multiplicado por 1
+    // Puntos : (1+1+1)*1 = 3
+    DistribucionViandas distribucion1 = DistribucionViandas.by(persona, LocalDate.of(2024, 3, 2),4);
+    DistribucionViandas distribucion2 = DistribucionViandas.by(persona, LocalDate.of(2024, 4, 2),10);
+    DistribucionViandas distribucion3 = DistribucionViandas.by(persona, LocalDate.of(2024, 5, 2),6);
 
-  @Test
-  @DisplayName("Los puntos por distribuir viandas es la cantidad de viandas distribuidas multiplicado por 1")
-  public void puntosDistribucionViandas() {
-  }
+    DistribucionViandasRepository.agregar(distribucion1);
+    DistribucionViandasRepository.agregar(distribucion2);
+    DistribucionViandasRepository.agregar(distribucion3);
 
-  @Test
-  @DisplayName("Los puntos por hacerse cargo de heladeras activas es la cantidad de heladeras activas multiplicado por la sumatoria de meses activa y por 5")
-  public void puntosHeladerasActivas() {
-  }
+    // Donacion Viandas. Los puntos por donar viandas es la cantidad de viandas donadas multiplicado por 1.5
+    // Puntos : (1+1+1)*1.5 = 4.5
+    DonacionVianda donacionVianda1 = DonacionVianda.by(persona, LocalDate.of(2024,5,6));
+    DonacionVianda donacionVianda2 = DonacionVianda.by(persona, LocalDate.of(2024,8,12));
+    DonacionVianda donacionVianda3 = DonacionVianda.by(persona, LocalDate.of(2024,1,8));
 
-  @Test
-  @DisplayName("Los puntos por donar viandas es la cantidad de viandas donadas multiplicado por 1.5")
-  public void puntosDonacionViandas() {
-  }
+    DonacionViandaRepository.agregar(donacionVianda1);
+    DonacionViandaRepository.agregar(donacionVianda2);
+    DonacionViandaRepository.agregar(donacionVianda3);
 
+    // Reparto Tarjetas. Los puntos por repartir tarjetas es la cantidad de tarjetas repartidas multiplicado por 2
+    // Puntos : (1+1+1)*2 = 6
+    RepartoDeTarjetas repartoDeTarjetas1 = RepartoDeTarjetas.by(persona, LocalDate.of(2024,5,5));
+    RepartoDeTarjetas repartoDeTarjetas2 = RepartoDeTarjetas.by(persona, LocalDate.of(2024,5,5));
+    RepartoDeTarjetas repartoDeTarjetas3 = RepartoDeTarjetas.by(persona, LocalDate.of(2024,5,5));
+
+    RepartoDeTarjetasRepository.agregar(repartoDeTarjetas1);
+    RepartoDeTarjetasRepository.agregar(repartoDeTarjetas2);
+    RepartoDeTarjetasRepository.agregar(repartoDeTarjetas3);
+
+    // Hacerse Cargo Heladera. Los puntos por hacerse cargo de heladeras activas es la cantidad de heladeras activas multiplicado por la sumatoria de meses activa y por 5
+    // Puntos : (1+1) * (4+6) * 5 = 100
+    Heladera heladera1 = Heladera.with(10);
+    Heladera heladera2 = Heladera.with(10);
+    Heladera heladera3 = Heladera.with(10);
+
+    heladera1.setEstado(EstadoHeladera.ACTIVA);
+    heladera2.setEstado(EstadoHeladera.INACTIVA);
+    heladera3.setEstado(EstadoHeladera.ACTIVA);
+
+    heladera1.setInicioFuncionamiento(LocalDate.of(2024, 3, 2));
+    heladera3.setInicioFuncionamiento(LocalDate.of(2024, 1, 1));
+
+    HacerseCargoHeladera hacerseCargoHeladera1 = HacerseCargoHeladera.with(persona, heladera1);
+    HacerseCargoHeladera hacerseCargoHeladera2 = HacerseCargoHeladera.with(persona, heladera2);
+    HacerseCargoHeladera hacerseCargoHeladera3 = HacerseCargoHeladera.with(persona, heladera3);
+
+    HacerseCargoHeladeraRepository.agregar(hacerseCargoHeladera1);
+    HacerseCargoHeladeraRepository.agregar(hacerseCargoHeladera2);
+    HacerseCargoHeladeraRepository.agregar(hacerseCargoHeladera3);
+
+  }
   @Test
   @DisplayName("Los puntos obtenidos de una persona es la sumatoria de los puntos por cada forma colaborada restado a sus puntos canjeados")
   public void puntosObtenidos() {
+    Assertions.assertEquals(calculadorDePuntos.calcularPuntos(), 5463.5);
   }
 }
+//  @Test
+//  @DisplayName("Los puntos por repartir tarjetas es la cantidad de tarjetas repartidas multiplicado por 2")
+//  public void puntosTarjetas() {
+//  }
+//
+//  @Test
+//  @DisplayName("Los puntos por donar dinero es la cantidad de dinero donado multiplicado por 0.5")
+//  public void puntosDonacionDinero() {
+//  }
+//
+//  @Test
+//  @DisplayName("Los puntos por distribuir viandas es la cantidad de viandas distribuidas multiplicado por 1")
+//  public void puntosDistribucionViandas() {
+//  }
+//
+//  @Test
+//  @DisplayName("Los puntos por hacerse cargo de heladeras activas es la cantidad de heladeras activas multiplicado por la sumatoria de meses activa y por 5")
+//  public void puntosHeladerasActivas() {
+//  }
+//
+//  @Test
+//  @DisplayName("Los puntos por donar viandas es la cantidad de viandas donadas multiplicado por 1.5")
+//  public void puntosDonacionViandas() {
+//  }
+
+
