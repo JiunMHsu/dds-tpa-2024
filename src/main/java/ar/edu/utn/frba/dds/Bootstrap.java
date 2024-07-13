7b8053b71d058b5c6c647319e3ab9b07aef43cbb
Jiun Ming Hsu <hsujm219@gmail.com>
2024-07-13 17:34:09 -0300

    test carga masiva y mail sender

--- src/main/java/ar/edu/utn/frba/dds/Bootstrap.java
+++ src/main/java/ar/edu/utn/frba/dds/Bootstrap.java
@@ -3,9 +3,5 @@ package ar.edu.utn.frba.dds;
 public class Bootstrap {
 
   public void bootsrap() {
-    // levantar las heladeras
-    // levantar los brokers
-    // setear los endpoints
-    // escuchar
   }
 }
--- src/test/java/ar/edu/utn/frba/dds/testMensajeria/TestMailSender.java
+++ src/test/java/ar/edu/utn/frba/dds/testMensajeria/TestMailSender.java
@@ -1,5 +1,19 @@
 package ar.edu.utn.frba.dds.testMensajeria;
 
+import ar.edu.utn.frba.dds.mensajeria.MailSender;
+import ar.edu.utn.frba.dds.models.data.Mail;
+import org.junit.jupiter.api.Test;
+
 public class TestMailSender {
 
+  @Test
+  public void testMailSender() {
+    MailSender mailSender = new MailSender(
+        "utn.dds.g22@gmail.com",
+        "wvyp iozi gzes lfxb",
+        "smtp.gmail.com",
+        "587");
+
+    mailSender.enviarMail(Mail.to("jhsu@frba.utn.edu.ar", "Test Mail Sender", "test"));
+  }
 }
