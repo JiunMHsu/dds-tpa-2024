8cbfc77038935decbda8b0fbc44ad243ece02e4d
Jiun Ming Hsu <hsujm219@gmail.com>
2024-08-16 08:20:00 -0300

    modifico suscripciones

--- src/main/java/ar/edu/utn/frba/dds/models/tarjeta/TarjetaColaborador.java
+++ src/main/java/ar/edu/utn/frba/dds/models/tarjeta/TarjetaColaborador.java
@@ -5,6 +5,7 @@ import ar.edu.utn.frba.dds.models.heladera.Heladera;
 import ar.edu.utn.frba.dds.models.heladera.SolicitudDeApertura;
 import ar.edu.utn.frba.dds.repository.heladera.SolicitudDeAperturaRepository;
 import ar.edu.utn.frba.dds.utils.GeneradorDeCodigosTarjeta;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -16,12 +17,30 @@ public class TarjetaColaborador implements Tarjeta {
 
   private String codigo;
   private Colaborador duenio;
+  private LocalDate fechaAlta;
+  private Boolean esActiva;
 
-  public static TarjetaColaborador with(Colaborador duenio) {
+
+  public static TarjetaColaborador of(String codigo,
+                                      Colaborador duenio,
+                                      LocalDate fechaAlta,
+                                      Boolean esActiva) {
+    return TarjetaColaborador
+        .builder()
+        .codigo(codigo)
+        .duenio(duenio)
+        .fechaAlta(fechaAlta)
+        .esActiva(esActiva)
+        .build();
+  }
+
+  public static TarjetaColaborador of(Colaborador duenio) {
     return TarjetaColaborador
         .builder()
         .codigo(GeneradorDeCodigosTarjeta.generar())
         .duenio(duenio)
+        .fechaAlta(LocalDate.now())
+        .esActiva(true)
         .build();
   }
 
@@ -33,6 +52,6 @@ public class TarjetaColaborador implements Tarjeta {
         .filter(s -> s.getHeladera().equals(heladera))
         .findFirst().orElse(null);
 
-    return solicitud != null;
+    return this.esActiva && (solicitud != null);
   }
 }
