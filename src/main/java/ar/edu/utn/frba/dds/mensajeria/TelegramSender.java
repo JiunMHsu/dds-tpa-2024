b831313200e10c5c7b96b05dcde459d99a57a101
Leonel Juncos Mieres <129603152+leoojuncos@users.noreply.github.com>
2024-08-22 19:30:19 -0300

    subo un par de cosas

--- src/main/java/ar/edu/utn/frba/dds/mensajeria/TelegramSender.java
+++ src/main/java/ar/edu/utn/frba/dds/mensajeria/TelegramSender.java
@@ -1,30 +1,59 @@
 package ar.edu.utn.frba.dds.mensajeria;
 
+import ar.edu.utn.frba.dds.AppConfig;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class TelegramSender implements Sender {
+  private final String botToken;
+  private String chatId; // Inicialmente null
+  private final CamelContext camelContext;
+  private final ProducerTemplate producerTemplate;
 
-  private final String AUTHORIZATION_TOKEN;
-  private final String CHAT_ID;
-  private CamelContext camelContext;
-
-  public TelegramSender(String authorizationToken, String chatID) {
-    this.AUTHORIZATION_TOKEN = authorizationToken;
-    this.CHAT_ID = chatID;
+  public TelegramSender() {
+    this.botToken = AppConfig.getProperty("TELEGRAM_AUTHORIZATION_TOKEN");
     this.camelContext = new DefaultCamelContext();
+    this.producerTemplate = camelContext.createProducerTemplate();
+    configurarCamelContext();
+  }
 
+  private void configurarCamelContext() {
     try {
       camelContext.addRoutes(new RouteBuilder() {
         @Override
         public void configure() {
-          from("direct:sendMessage")
-                  .toF("telegram:bots/?authorizationToken=%s&chatId=%s", AUTHORIZATION_TOKEN, CHAT_ID);
+          // Ruta para obtener el chatId
+          from("telegram:bots?authorizationToken=" + botToken)
+                  .to("log:INFO?showHeaders=true")
+                  .process(exchange -> {
+                    // Obtener el chatId de los headers
+                    String receivedChatId = exchange.getIn().getHeader("CamelTelegramChatId", String.class);
+                    if (receivedChatId != null) {
+                      chatId = receivedChatId;
+                      System.out.println("Chat ID obtenido: " + chatId);
+                    }
+                  });
+
+          // Ruta para enviar mensajes, utiliza el chatId cuando está disponible
+          from("direct:sendToTelegram")
+                  .choice()
+                  .when(exchange -> chatId != null)
+                  .to("telegram:bots?authorizationToken=" + botToken + "&chatId=" + chatId)
+                  .otherwise()
+                  .log("Chat ID no disponible. No se puede enviar el mensaje.");
         }
       });
       camelContext.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void obtenerChatId() {
+    try {
+      producerTemplate.sendBody("telegram:bots?authorizationToken=" + botToken, "Obteniendo Chat ID");
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -39,12 +68,5 @@ public class TelegramSender implements Sender {
       e.printStackTrace();
     }
   }
-
-  public void stop() {
-    try {
-      camelContext.stop();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 }
+
--- src/test/java/ar/edu/utn/frba/dds/testMensajeria/TestTelegram.java
+++ src/test/java/ar/edu/utn/frba/dds/testMensajeria/TestTelegram.java
@@ -4,12 +4,15 @@ import ar.edu.utn.frba.dds.mensajeria.TelegramSender;
 import org.junit.jupiter.api.Test;
 
 public class TestTelegram {
+
     @Test
     public void enviarTelegram() {
-        TelegramSender telegramSender = new TelegramSender(
-                "7528108332:AAH5sYAJW-xnd0EVQqaPAXaN1DSY7A6qAkE",
-                "");
-
+        TelegramSender telegramSender = new TelegramSender();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         telegramSender.enviarMensaje("", "test", "Test - Grupo 22");
     }
 }
