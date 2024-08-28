aadf84df58a4ad22cd0742089704ce642a1ad685
Jiun Ming Hsu <hsujm219@gmail.com>
2024-08-28 17:29:18 -0300

    completo broker

--- src/main/java/ar/edu/utn/frba/dds/broker/suscriptorSensor/SuscriptorSensorMovimiento.java
+++ src/main/java/ar/edu/utn/frba/dds/broker/suscriptorSensor/SuscriptorSensorMovimiento.java
@@ -1,16 +1,16 @@
 package ar.edu.utn.frba.dds.broker.suscriptorSensor;
 
-import ar.edu.utn.frba.dds.broker.ClienteMqtt;
 import ar.edu.utn.frba.dds.models.heladera.Sensor;
 
 public class SuscriptorSensorMovimiento extends SuscriptorSensor {
 
-  public SuscriptorSensorMovimiento(ClienteMqtt cliente, Sensor suscriptor) {
-    super(cliente, suscriptor);
+  public SuscriptorSensorMovimiento(Sensor suscriptor) {
+    super(suscriptor);
   }
 
   // TODO (delegar a controller? o este mismo cumple el rol de controller?)
   @Override
   public void recibirMensaje(String mensaje) {
   }
+
 }
--- src/test/java/ar/edu/utn/frba/dds/TestBroker.java
+++ src/test/java/ar/edu/utn/frba/dds/TestBroker.java
@@ -2,19 +2,25 @@ package ar.edu.utn.frba.dds;
 
 import ar.edu.utn.frba.dds.broker.ClienteMqtt;
 import ar.edu.utn.frba.dds.broker.SuscriptorMqtt;
+import ar.edu.utn.frba.dds.broker.suscriptorSensor.SuscriptorSensorTemperatura;
+import ar.edu.utn.frba.dds.models.heladera.Sensor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestBroker {
 
-  private SuscriptorMqtt unSuscriptor;
   private ClienteMqtt cliente;
   private final String topic = "dds/g22/test";
 
   @BeforeEach
   public void setUp() {
+    cliente = new ClienteMqtt();
+  }
 
+  @Test
+  @DisplayName("El Suscriptor puede recibir correctamente los mensajes publicados en el topic específico")
+  public void testSuscripcion() throws InterruptedException {
     class SuscriptorTest implements SuscriptorMqtt {
       @Override
       public String topic() {
@@ -27,13 +33,7 @@ public class TestBroker {
       }
     }
 
-    unSuscriptor = new SuscriptorTest();
-    cliente = new ClienteMqtt();
-  }
-
-  @Test
-  @DisplayName("El Suscriptor puede recibir correctamente los mensajes publicados en el topic específico")
-  public void testSuscripcion() throws InterruptedException {
+    SuscriptorMqtt unSuscriptor = new SuscriptorTest();
     cliente.suscribirPara(unSuscriptor);
     ClienteMqtt clientePublicador = new ClienteMqtt();
 
@@ -43,4 +43,13 @@ public class TestBroker {
     }
   }
 
+  @Test
+  public void mensajeFueraDeTiempo() throws InterruptedException {
+    SuscriptorSensorTemperatura unSuscriptor = SuscriptorSensorTemperatura.para(Sensor.de(topic), 3000, 1);
+    ClienteMqtt clientePublicador = new ClienteMqtt();
+    cliente.suscribirPara(unSuscriptor);
+    clientePublicador.publicarMensaje(topic, "dds-test");
+
+    Thread.sleep(5000);
+  }
 }
