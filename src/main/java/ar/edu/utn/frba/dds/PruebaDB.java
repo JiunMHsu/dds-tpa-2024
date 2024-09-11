76a3c9fb8602df901d6893f6ca5c085acf9d6981
Jiun Ming Hsu <hsujm219@gmail.com>
2024-09-11 20:11:24 -0300

    intentando generar la base

---
+++ src/main/java/ar/edu/utn/frba/dds/PruebaDB.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.frba.dds;
+
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+
+public class PruebaDB implements WithSimplePersistenceUnit {
+  public static void main(String[] args) {
+    PruebaDB instance = new PruebaDB();
+    instance.impactarEnBase();
+  }
+
+  public void impactarEnBase() {
+
+    withTransaction(() -> {
+    });
+  }
+}
