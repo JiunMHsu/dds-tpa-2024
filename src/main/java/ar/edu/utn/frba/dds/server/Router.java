4b1fae14e41450b1e4e2878c1ea0577455b9beee
Jiun Ming Hsu <hsujm219@gmail.com>
2024-10-03 01:11:25 -0300

    dos forms de colaboraciones

--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -9,11 +9,19 @@ public class Router {
 
         app.get("/home", ctx -> ctx.render("home/home.hbs"));
 
-        app.get("/heladeras/nueva", ctx -> ctx.render("heladeras/heladera_crear.hbs"));
+        app.get("/heladeras/nuevo", ctx -> ctx.render("heladeras/heladera_crear.hbs"));
 
         app.get("/heladeras/unId", ctx -> ctx.render("heladeras/heladera_detalle.hbs"));
 
         app.get("/heladeras", ctx -> ctx.render("heladeras/heladeras.hbs"));
+
+        app.get("/colaboraciones/donacion-dinero/nuevo", ctx -> ctx.render("colaboraciones/donacion_dinero_crear.hbs"));
+        app.get("/colaboraciones/donacion-vianda/nuevo", ctx -> ctx.result("PENDIENTE"));
+        app.get("/colaboraciones/registro-persona-vulnerable/nuevo", ctx -> ctx.result("PENDIENTE"));
+        app.get("/colaboraciones/entrega-viandas/nuevo", ctx -> ctx.result("PENDIENTE"));
+        app.get("/colaboraciones/distribucion-viandas/nuevo", ctx -> ctx.render("colaboraciones/distribucion_viandas_crear.hbs"));
+        app.get("/colaboraciones/oferta-producto-servicio/nuevo", ctx -> ctx.result("PENDIENTE"));
+        app.get("/colaboraciones/tomar-cargo-heladeras/nuevo", ctx -> ctx.result("PENDIENTE"));
 
         app.get("/colaboraciones", ctx -> ctx.render("colaboraciones/colaboraciones.hbs"));
 
