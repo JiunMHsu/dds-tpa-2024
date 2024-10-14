664bf304fb393b23b0ebf31357340ce8dfe4c79f
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-10-14 07:59:24 -0300

    oferta por id en controller

---
+++ src/main/java/ar/edu/utn/frba/dds/controllers/productosServicios/ProductosServiciosController.java
@@ -0,0 +1,58 @@
+package ar.edu.utn.frba.dds.controllers.productosServicios;
+
+import ar.edu.utn.frba.dds.models.entities.colaboracion.OfertaDeProductos;
+import ar.edu.utn.frba.dds.models.repositories.colaboracion.OfertaDeProductosRepository;
+import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
+import io.javalin.http.Context;
+
+import java.util.Optional;
+
+public class ProductosServiciosController implements ICrudViewsHandler {
+
+    private OfertaDeProductosRepository ofertaDeProductosRepository;
+
+    public ProductosServiciosController(OfertaDeProductosRepository ofertaDeProductosRepository) {
+        this.ofertaDeProductosRepository = ofertaDeProductosRepository;
+    }
+
+    @Override
+    public void index(Context context){
+
+    }
+    @Override
+    public void show(Context context){
+
+    }
+    @Override
+    public void create(Context context){
+
+    }
+    @Override
+    public void save(Context context){
+
+    }
+    @Override
+    public void edit(Context context){
+
+    }
+    @Override
+    public void update(Context context){
+
+    }
+    @Override
+    public void delete(Context context){
+
+    }
+
+    public OfertaDeProductos ofertaPorId(String id){
+        Optional<OfertaDeProductos> ofertaBuscada = ofertaDeProductosRepository.buscarPorId(id);
+        //TODO manejar empty
+
+        return ofertaBuscada.get();
+    }
+
+
+
+
+
+}
