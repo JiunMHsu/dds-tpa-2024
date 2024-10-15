2be1bc1c18e7a6600f9fef08be342c18fa911b11
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-10-15 02:22:31 -0300

    save hacerse cargo heladera

---
+++ src/main/java/ar/edu/utn/frba/dds/controllers/colaboraciones/HacerseCargoHeladeraController.java
@@ -0,0 +1,63 @@
+package ar.edu.utn.frba.dds.controllers.colaboraciones;
+
+import ar.edu.utn.frba.dds.models.entities.colaboracion.HacerseCargoHeladera;
+import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.repositories.colaboracion.HacerseCargoHeladeraRepository;
+import ar.edu.utn.frba.dds.models.repositories.colaborador.ColaboradorRepository;
+import ar.edu.utn.frba.dds.models.repositories.heladera.HeladeraRepository;
+import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
+import io.javalin.http.Context;
+
+import java.time.LocalDateTime;
+
+public class HacerseCargoHeladeraController implements ICrudViewsHandler {
+
+    private HacerseCargoHeladeraRepository hacerseCargoHeladeraRepository;
+    private ColaboradorRepository colaboradorRepository;
+
+    private HeladeraRepository heladeraRepository;
+
+
+    public HacerseCargoHeladeraController(HacerseCargoHeladeraRepository hacerseCargoHeladeraRepository) {
+        this.hacerseCargoHeladeraRepository = hacerseCargoHeladeraRepository;
+    }
+
+    @Override
+    public void index(Context context){
+
+    }
+    @Override
+    public void show(Context context){
+
+    }
+    @Override
+    public void create(Context context){
+        context.render("colaboraciones/encargarse_de_heladera_crear.hbs");
+    }
+    @Override
+    public void save(Context context){
+        Colaborador colaborador = colaboradorRepository.buscarPorId(context.sessionAttribute("userId")).get();
+        //TODO chequear empty heladeras
+        Heladera heladeraACargo = heladeraRepository.buscarPorId(context.formParam("heladera_origen")).get();
+        HacerseCargoHeladera hacerseCargoHeladera = HacerseCargoHeladera.por(colaborador, LocalDateTime.now(),heladeraACargo);
+
+        this.hacerseCargoHeladeraRepository.guardar(hacerseCargoHeladera);
+
+        context.redirect("result_form");
+
+    }
+    @Override
+    public void edit(Context context){
+
+    }
+    @Override
+    public void update(Context context){
+
+    }
+    @Override
+    public void delete(Context context){
+
+    }
+}
+
