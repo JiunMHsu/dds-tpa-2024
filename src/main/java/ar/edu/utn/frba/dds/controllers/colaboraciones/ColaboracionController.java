cc76a62a611f8ef15b6a682fda1aa9130941cefe
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-10-15 02:29:59 -0300

    colaboracion vacio

---
+++ src/main/java/ar/edu/utn/frba/dds/controllers/colaboraciones/ColaboracionController.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.controllers.colaboraciones;
+
+import io.javalin.http.Context;
+
+public class ColaboracionController {
+
+    public void index(Context context){
+
+    }
+}
