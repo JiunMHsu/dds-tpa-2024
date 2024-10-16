d6b58a8362b2ae06715f6e22ea69d828ae7c368d
Jiun Ming Hsu <hsujm219@gmail.com>
2024-10-16 14:38:03 -0300

    auth middleware

--- src/main/java/ar/edu/utn/frba/dds/middlewares/AuthMiddleware.java
+++ src/main/java/ar/edu/utn/frba/dds/middlewares/AuthMiddleware.java
@@ -1,11 +1,32 @@
 package ar.edu.utn.frba.dds.middlewares;
 
+import ar.edu.utn.frba.dds.exceptions.UnauthenticatedException;
+import ar.edu.utn.frba.dds.exceptions.UnauthorizedException;
+import ar.edu.utn.frba.dds.models.entities.rol.TipoRol;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.security.RouteRole;
+import java.util.Set;
 
 public class AuthMiddleware {
 
     public static void apply(Javalin app) {
+        app.beforeMatched(context -> {
+            TipoRol userRol = rolFromSession(context);
+
+            if (userRol == null)
+                throw new UnauthenticatedException("unauthenticated");
+
+            Set<RouteRole> routeRoles = context.routeRoles();
+            if (!routeRoles.isEmpty() && !routeRoles.contains(userRol))
+                throw new UnauthorizedException("unauthorized");
+
+        });
     }
 
-    // TODO - Método para recuperar Rol según context
+    private static TipoRol rolFromSession(Context context) {
+        return context.sessionAttribute("userRol") != null
+                ? TipoRol.valueOf(context.sessionAttribute("userRol"))
+                : null;
+    }
 }
