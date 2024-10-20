f3ffce051a9a6d1f910633458938feaa8949ff3b
Jiun Ming Hsu <hsujm219@gmail.com>
2024-10-20 10:24:26 -0300

    routing colaborador

--- src/main/java/ar/edu/utn/frba/dds/server/routers/ColaboradorRouter.java
+++ src/main/java/ar/edu/utn/frba/dds/server/routers/ColaboradorRouter.java
@@ -1,11 +1,34 @@
 package ar.edu.utn.frba.dds.server.routers;
 
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
 import io.javalin.config.JavalinConfig;
 
 public class ColaboradorRouter implements IRouter {
 
     @Override
     public void apply(JavalinConfig config) {
-        // TODO - endpoint colaboradores
+        config.router.apiBuilder(() ->
+                path("/colaboradores", () -> {
+                    get(ctx -> ctx.result("GET Colaboradores"));
+                    post(ctx -> ctx.result("POST Colaboradores"));
+
+                    // get("/new", ctx -> ctx.result("FORM REGISTRO")); (es el sign up)
+                    path("/{id}", () -> {
+                        get(ctx -> ctx.result("GET Colaborador " + ctx.pathParam("id")));
+                        post(ctx -> ctx.result("POST Colaborador " + ctx.pathParam("id")));
+
+                        get("/edit", ctx -> ctx.result("GET Form edit"));
+
+                        path("/formas-de-colaboracion", () -> {
+                            post(ctx -> ctx.result("POST Formas de Colaboracion"));
+                            get("/edit", ctx -> ctx.result("GET Form edit"));
+                        });
+
+                    });
+                })
+        );
     }
 }
