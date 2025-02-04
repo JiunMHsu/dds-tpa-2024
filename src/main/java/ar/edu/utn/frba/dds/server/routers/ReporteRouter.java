77d9114a9bae32e8ae13ca6b3e74985700101b24
Jiun Ming Hsu <hsujm219@gmail.com>
2025-02-04 08:58:20 -0300

    refactor generado de reportes

--- src/main/java/ar/edu/utn/frba/dds/server/routers/ReporteRouter.java
+++ src/main/java/ar/edu/utn/frba/dds/server/routers/ReporteRouter.java
@@ -2,20 +2,33 @@ package ar.edu.utn.frba.dds.server.routers;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.post;
 
 import ar.edu.utn.frba.dds.config.ServiceLocator;
 import ar.edu.utn.frba.dds.controllers.reporte.ReporteController;
 import ar.edu.utn.frba.dds.models.entities.usuario.TipoRol;
 import io.javalin.config.RouterConfig;
 
+/**
+ * Router de reportes.
+ */
 public class ReporteRouter implements IRouter {
 
   @Override
   public void apply(RouterConfig config) {
     config.apiBuilder(() ->
         path("/reportes", () -> {
-          get(ServiceLocator.instanceOf(ReporteController.class)::index, TipoRol.ADMIN);
-          get("/{id}/*", ServiceLocator.instanceOf(ReporteController.class)::show, TipoRol.ADMIN);
+          get(ServiceLocator.instanceOf(ReporteController.class)::index,
+              TipoRol.ADMIN,
+              TipoRol.COLABORADOR);
+          
+          post(ServiceLocator.instanceOf(ReporteController.class)::generate,
+              TipoRol.ADMIN,
+              TipoRol.COLABORADOR);
+
+          get("/{id}/*", ServiceLocator.instanceOf(ReporteController.class)::show,
+              TipoRol.ADMIN,
+              TipoRol.COLABORADOR);
         })
     );
   }
