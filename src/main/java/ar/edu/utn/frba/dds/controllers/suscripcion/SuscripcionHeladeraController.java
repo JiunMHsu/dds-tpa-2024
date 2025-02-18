630beae28e41e7a29ecef8bbddc494bc0adf88a6
Melselep <130233474+Melselep@users.noreply.github.com>
2025-02-18 11:24:10 -0300

    documentacion suscripcion-controller

--- src/main/java/ar/edu/utn/frba/dds/controllers/suscripcion/SuscripcionHeladeraController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/suscripcion/SuscripcionHeladeraController.java
@@ -25,7 +25,9 @@ import java.util.List;
 import java.util.Map;
 
 
-// TODO - documentar
+/**
+ * Controlador para manejar las suscripciones relacionadas con las heladeras.
+ */
 public class SuscripcionHeladeraController extends ColaboradorRequired {
 
   private final HeladeraService heladeraService;
@@ -33,6 +35,16 @@ public class SuscripcionHeladeraController extends ColaboradorRequired {
   private final FaltaViandaService faltaViandaService;
   private final HeladeraLlenaService heladeraLlenaService;
 
+  /**
+   * Constructor para inicializar los servicios necesarios para manejar las suscripciones.
+   *
+   * @param heladeraService       El servicio de heladeras.
+   * @param fallaHeladeraService  El servicio de fallas de heladeras.
+   * @param faltaViandaService    El servicio de falta de viandas.
+   * @param heladeraLlenaService  El servicio de heladeras llenas.
+   * @param usuarioService        El servicio de usuarios.
+   * @param colaboradorService    El servicio de colaboradores.
+   */
   public SuscripcionHeladeraController(HeladeraService heladeraService,
                                        FallaHeladeraService fallaHeladeraService,
                                        FaltaViandaService faltaViandaService,
@@ -46,6 +58,12 @@ public class SuscripcionHeladeraController extends ColaboradorRequired {
     this.heladeraLlenaService = heladeraLlenaService;
   }
 
+  /**
+   * Crea una suscripción a una heladera específica y renderiza la vista de suscripción.
+   *
+   * @param context         El contexto de la solicitud actual.
+   * @param pathSuscripcion La ruta de la vista de suscripción.
+   */
   public void createSuscripcion(Context context, String pathSuscripcion) {
 
     String heladeraId = context.queryParamAsClass("heladera", String.class).get();
@@ -60,22 +78,52 @@ public class SuscripcionHeladeraController extends ColaboradorRequired {
     render(context, "suscripciones/" + pathSuscripcion, model);
   }
 
+  /**
+   * Crea una suscripción genérica a una heladera y renderiza la vista de suscripción.
+   *
+   * @param context  El contexto de la solicitud actual.
+   */
   public void create(Context context) {
     createSuscripcion(context, "suscripciones.hbs");
   }
 
+  /**
+   * Crea una suscripción a las fallas de heladera y renderiza la vista de suscripción de fallas.
+   *
+   * @param context  El contexto de la solicitud actual.
+   */
   public void createFallaHeladera(Context context) {
     this.createSuscripcion(context, "suscripcion_falla_heladera.hbs");
   }
 
+  /**
+   * Crea una suscripción a la falta de viandas y renderiza la vista de suscripción de falta de viandas.
+   *
+   * @param context  El contexto de la solicitud actual.
+   */
   public void createFaltaVianda(Context context) {
     this.createSuscripcion(context, "suscripcion_falta_viandas.hbs");
   }
 
+  /**
+   * Crea una suscripción a heladera llena y renderiza la vista de suscripción de heladera llena.
+   *
+   * @param context  El contexto de la solicitud actual.
+   */
   public void createHeladeraLlena(Context context) {
     this.createSuscripcion(context, "suscripcion_heladera_llena.hbs");
   }
 
+  /**
+   * Guarda una nueva suscripción en el sistema y maneja la redirección en función del éxito de la operación.
+   * Este método extrae los parámetros necesarios del formulario y del contexto de la solicitud,
+   * crea un nuevo objeto `CreateSuscripcionHeladeraDTO` y lo registra en el servicio correspondiente
+   * según el tipo de suscripción (falla técnica, falta de viandas, o heladera llena).
+   * Si la operación es exitosa, redirige al usuario a la lista de heladeras. En caso de error,
+   * redirige al usuario para que reintente la creación de la suscripción.
+   * @param context  El contexto de la solicitud actual.
+   * @throws UnauthorizedException si el usuario no es colaborador.
+   */
   public void save(Context context) {
 
     Map<String, Object> model = new HashMap<>();
