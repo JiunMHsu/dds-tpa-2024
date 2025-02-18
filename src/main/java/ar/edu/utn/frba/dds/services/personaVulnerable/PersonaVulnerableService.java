7c50e117e590321acae078d580e50297ad1d267d
Melselep <130233474+Melselep@users.noreply.github.com>
2025-02-18 11:50:43 -0300

    documentacion personaVulnerable-service

--- src/main/java/ar/edu/utn/frba/dds/services/personaVulnerable/PersonaVulnerableService.java
+++ src/main/java/ar/edu/utn/frba/dds/services/personaVulnerable/PersonaVulnerableService.java
@@ -8,20 +8,40 @@ import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Servicio para manejar las operaciones relacionadas con personas vulnerables.
+ */
 @Getter
 @Setter
 public class PersonaVulnerableService {
 
   private final PersonaVulnerableRepository personaVulnerableRepository;
 
+  /**
+   * Constructor para inicializar el repositorio de personas vulnerables.
+   *
+   * @param personaVulnerableRepository El repositorio de personas vulnerables.
+   */
   public PersonaVulnerableService(PersonaVulnerableRepository personaVulnerableRepository) {
     this.personaVulnerableRepository = personaVulnerableRepository;
   }
 
+  /**
+   * Busca todas las personas vulnerables en el repositorio.
+   *
+   * @return Una lista de todas las personas vulnerables.
+   */
   public List<PersonaVulnerable> buscarTodosPV() {
     return this.personaVulnerableRepository.buscarTodos();
   }
 
+  /**
+   * Busca una persona vulnerable por su ID.
+   *
+   * @param id El ID de la persona vulnerable.
+   * @return Un Optional que contiene la persona vulnerable si se encuentra.
+   * @throws IllegalArgumentException si el ID es null o vacío.
+   */
   public Optional<PersonaVulnerable> buscarPVPorId(String id) {
 
     if (id == null || id.isEmpty()) {
@@ -31,6 +51,13 @@ public class PersonaVulnerableService {
     return this.personaVulnerableRepository.buscarPorId(id);
   }
 
+  /**
+   * Guarda una nueva persona vulnerable en el repositorio.
+   *
+   * @param personaVulnerable La persona vulnerable a guardar.
+   * @throws IllegalArgumentException si los datos de la persona vulnerable son incompletos
+   * o si el documento ya está registrado en el sistema.
+   */
   public void guardarPV(PersonaVulnerable personaVulnerable) {
 
     System.out.println("Antes del if documento service pv");
@@ -55,6 +82,12 @@ public class PersonaVulnerableService {
     this.personaVulnerableRepository.guardar(personaVulnerable);
   }
 
+  /**
+   * Elimina una persona vulnerable del repositorio.
+   *
+   * @param id El ID de la persona vulnerable a eliminar.
+   * @throws IllegalArgumentException si el ID es null o vacío o si la persona no existe en el sistema.
+   */
   public void eliminarPV(String id) {
 
     if (id == null || id.isEmpty()) {
@@ -71,6 +104,14 @@ public class PersonaVulnerableService {
     // Deberia haber aplicar alguna logica sobre las tarjetas vinculadas a la PV?
   }
 
+
+  /**
+   * Actualiza los datos de una persona vulnerable existente en el repositorio.
+   *
+   * @param id    El ID de la persona vulnerable a actualizar.
+   * @param input Los nuevos datos de la persona vulnerable.
+   * @throws ResourceNotFoundException si la persona vulnerable no se encuentra en el sistema.
+   */
   public void actualizarPersonaVulnerable(String id, PersonaVulnerable input) {
     PersonaVulnerable personaVulnerable = personaVulnerableRepository
         .buscarPorId(id).orElseThrow(ResourceNotFoundException::new);
